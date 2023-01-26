208f131d71a19b9681e4dadd6bb5cf256bc4eeae
xuyuchao <2672424338@qq.com>
2023-01-26 22:06:46 +0800

    完善comment接口 xyc

--- model/comment.go
+++ model/comment.go
@@ -33,3 +33,22 @@ func (c *commentMan) AddComment(comment *Comment) error {
 	}
 	return nil
 }
+
+// QueryCommentById 根据评论ID查询评论信息方法
+func (c *commentMan) QueryCommentById(id int64, comment *Comment) error {
+	if comment == nil {
+		return errors.New("QueryCommentById comment 空指针")
+	}
+	return c.db.Where("id=?", id).First(comment).Error
+}
+
+// QueryCommentListByVideoId 根据视频ID查询评论列表
+func (c *commentMan) QueryCommentListByVideoId(videoId int64, comments *[]Comment) error {
+	if comments == nil {
+		return errors.New("QueryCommentListByVideoId comments空指针")
+	}
+	if err := c.db.Model(&Comment{}).Where("video_id=?", videoId).Find(comments).Error; err != nil {
+		return err
+	}
+	return nil
+}
--- model/comment_test.go
+++ model/comment_test.go
@@ -1,6 +1,7 @@
 package model_test
 
 import (
+	"fmt"
 	"github.com/HumXC/simple-douyin/model"
 	"path"
 	"testing"
@@ -18,16 +19,39 @@ func TestComment(t *testing.T) {
 		t.Fatal(err)
 	}
 	commentMan := douyinDB.Comment
-	comment := &model.Comment{
+	_ = &model.Comment{
 		UserID:  1,
 		VideoId: 1,
 		Content: "测试评论1",
 	}
+	comment2 := &model.Comment{
+		UserID:  2,
+		VideoId: 1,
+		Content: "测试评论2",
+	}
 
-	t.Run("添加评论测试", func(t *testing.T) {
-		err := commentMan.AddComment(comment)
+	t.Run("AddComment", func(t *testing.T) {
+		err := commentMan.AddComment(comment2)
 		if err != nil {
 			t.Fatal(err)
 		}
+	})
+
+	t.Run("QueryCommentById", func(t *testing.T) {
+		var comment model.Comment
+		err := commentMan.QueryCommentById(2, &comment)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Println(comment)
+	})
+
+	t.Run("QueryCommentListByVideoId", func(t *testing.T) {
+		var comments []model.Comment
+		err := commentMan.QueryCommentListByVideoId(1, &comments)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Println(comments)
 	})
 }
