a5ace4453fb1f7c3acbcbb222cb6ac0f3bb66bad
xuyuchao <2672424338@qq.com>
2023-01-27 22:46:24 +0800

    拦截请求并设置user_id在请求参数上

--- middlewares/auth_user.go
+++ middlewares/auth_user.go
@@ -4,12 +4,14 @@ import (
 	"github.com/HumXC/simple-douyin/handler/douyin"
 	"github.com/HumXC/simple-douyin/helper"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 )
 
 func AuthUserCheck() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		auth := c.Query("token")
+		log.Println("用户token => {}", auth)
 		userClaim, err := helper.AnalyseToken(auth)
 		if err != nil {
 			c.Abort()
@@ -20,6 +22,7 @@ func AuthUserCheck() gin.HandlerFunc {
 			return
 		}
 		c.Set("user", userClaim)
+		c.Set("user_id", userClaim.UserId)
 		c.Next()
 	}
 }
