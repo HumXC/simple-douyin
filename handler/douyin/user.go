9a0c4235683c2ea7e9d3aeb3dfdfd08965d13f09
yn8886 <1219156469@qq.com>
2023-02-13 22:57:33 +0800

    实现登录注册功能

--- handler/douyin/user.go
+++ handler/douyin/user.go
@@ -2,15 +2,19 @@ package douyin
 
 import (
 	"net/http"
+	"unicode/utf8"
 
+	"github.com/HumXC/simple-douyin/helper"
+	"github.com/HumXC/simple-douyin/model"
 	"github.com/gin-gonic/gin"
 )
 
 type UserLoginResponse struct {
 	Response
-	UserId uint   `json:"user_id"`
-	Token  string `json:"token"`
+	UserId 	  int64  `json:"user_id"`
+	Token  	  string `json:"token"`
 }
+
 
 // FIXME： 并未实现登录功能，只是为了方便测试
 func (h *Handler) User(c *gin.Context) {
@@ -18,7 +22,7 @@ func (h *Handler) User(c *gin.Context) {
 		Response
 		User
 	}
-	c.JSON(200, resp{
+	c.JSON(http.StatusOK, resp{
 		Response: Response{
 			StatusCode: StatusOK,
 			StatusMsg:  "OK",
@@ -26,14 +30,93 @@ func (h *Handler) User(c *gin.Context) {
 		User: User{},
 	})
 }
+
 func (h *Handler) UserLogin(c *gin.Context) {
+	userMan := h.DB.User
+	username := c.Query("username")
+	password := c.Query("password")
+
+	//用户不存在
+	if ok := userMan.UserIsExistByName(username); !ok {
+		ResponseError(c, "用户不存在")
+		return
+	}
+	//验证用户名和密码
+	if err := userMan.CheckNameAndPwd(username, password); err != nil {
+		ResponseError(c, err.Error())
+		return
+	}
+	//获取user_id
+	userId := userMan.GetUserIdByName(username)
+	//生成token
+	token ,err := helper.GenerateToken(userId)
+	if err != nil {
+		ResponseError(c, err.Error())
+	}
 	resp := UserLoginResponse{
 		Response: Response{
 			StatusCode: 0,
 			StatusMsg:  "登录成功",
 		},
-		UserId: 0,
-		Token:  "testUser",
+		UserId: userId,
+		Token:  token,
 	}
 	c.JSON(http.StatusOK, resp)
+}
+
+func (h *Handler) UserRegister(c *gin.Context) {
+	userMan := h.DB.User
+	username := c.Query("username")
+	password := c.Query("password")
+	
+	//用户名存在
+	if ok := userMan.UserIsExistByName(username); ok {
+		ResponseError(c, "用户已存在")
+		return
+	}
+	//用户名或密码为空
+	if username == "" || password == "" {
+		ResponseError(c, "用户名（或密码）为空")
+		return
+	}
+	//用户名或密码长度超出限制
+	if utf8.RuneCount([]byte(username)) > 32 || utf8.RuneCount([]byte(password)) > 32 {
+		ResponseError(c, "用户名（或密码）长度超过限制")
+		return
+	}
+	//保存到数据库
+	user := model.User{
+		Name: username,
+		Password: password,
+	}
+	if err := userMan.AddUser(&user); err != nil {
+		ResponseError(c, err.Error())
+		return
+	}
+	//生成user_id和token
+	userId := user.Id
+	token, err := helper.GenerateToken(userId)
+	if err != nil {
+		ResponseError(c, err.Error())
+		return
+	}
+
+	resp := UserLoginResponse{
+		Response: Response{
+			StatusCode: 0,
+			StatusMsg:  "注册成功",
+		},
+		UserId: userId,
+		Token:  token,
+	}
+	c.JSON(http.StatusOK, resp)
+}
+
+func ResponseError(c *gin.Context, msg string) {
+	c.JSON(http.StatusOK, UserLoginResponse{
+		Response: Response{
+			StatusCode: -1,
+			StatusMsg: msg,
+		},
+	})
 }
--- model/user.go
+++ model/user.go
@@ -9,16 +9,32 @@ import (
 type User struct {
 	Id             int64      `json:"id,omitempty"`
 	Name           string     `json:"name,omitempty"`
+	Password	   string  	  `json:"password,omitempty"`
 	FollowCount    int64      `json:"follow_count,omitempty"`
 	FollowerCount  int64      `json:"follower_count,omitempty"`
 	IsFollow       bool       `json:"is_follow,omitempty"`
 	TotalFavorited int64      `json:"total_favorited,omitempty"`
 	FavoriteCount  int64      `json:"favorite_count,omitempty"`
-	UserLogin      *UserLogin `json:"user_login" gorm:"foreignkey:UserId"`
 }
 
 type userMan struct {
 	db *gorm.DB
+}
+
+func (u *userMan) GetUserIdByName(name string) (userId int64) {
+	user := User{}
+	u.db.Where("name=?", name).First(&user)
+	return user.Id
+}
+
+func (u *userMan) UserIsExistByName(name string) bool {
+	err := u.db.Where("name=?", name).First(&User{}).Error
+	return err == nil
+}
+
+func (u *userMan) CheckNameAndPwd(name string, password string) error {
+	err := u.db.Where("name=? and password=?", name, password).First(&User{}).Error
+	return err
 }
 
 func (u *userMan) AddUser(user *User) error {
@@ -26,6 +42,7 @@ func (u *userMan) AddUser(user *User) error {
 		return errors.New("AddUser user空指针")
 	}
 	//注册用户
-	err := u.db.Create(user).Error
-	return err
+	return u.db.Create(user).Error
 }
+
+
