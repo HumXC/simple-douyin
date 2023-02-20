5573b93ada207eec60e7724382776877e3c05f73
xuyuchao <2672424338@qq.com>
2023-02-20 20:43:05 +0800

    简单实现拉取消息列表

--- handler/douyin/message.go
+++ handler/douyin/message.go
@@ -45,5 +45,46 @@ func (h *Handler) MessageAction(c *gin.Context) {
 }
 
 func (h *Handler) MessageChatListAction(c *gin.Context) {
+	type Resp struct {
+		Response
+		MessageList []Message ` json:"message_list,omitempty"`
+	}
+	resp := Resp{
+		Response: BaseResponse(),
+	}
+	defer func() {
+		c.JSON(http.StatusOK, resp)
+	}()
+	messageMan := h.DB.Message
+	fromUserId := c.GetInt64("user_id")
+	toUserId, _ := strconv.Atoi(c.Query("to_user_id")) //对方用户ID
+	var messages []model.Message
 
+	var messages1 []model.Message
+	var messages2 []model.Message
+
+	err := messageMan.QueryMessageRecord(fromUserId, int64(toUserId), &messages1)
+	err = messageMan.QueryMessageRecord(int64(toUserId), fromUserId, &messages2)
+
+	messages = append(messages1, messages2...)
+
+	if err != nil {
+		resp.Status(StatusFailedChatList)
+		log.Println("拉取聊天记录失败", err.Error())
+		return
+	}
+	messageList := make([]Message, len(messages))
+	idx := 0
+	for _, message := range messages {
+		messageData := Message{
+			Id:         int64(message.Model.ID),
+			ToUserId:   message.ToUserId,
+			FromUserId: message.FromUserId,
+			Content:    message.Content,
+			CreateTime: message.CreatedAt.Format("2006-01-02 15:04:05"),
+		}
+		messageList[idx] = messageData
+		idx = idx + 1
+	}
+	resp.MessageList = messageList
 }
