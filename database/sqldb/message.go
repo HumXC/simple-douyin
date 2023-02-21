0b0eef6bac06c449cc96fd228a564a7d8e285ca3
HumXC <Hum-XC@outlook.com>
2023-02-21 22:44:24 +0800

    将 database/sqldb 的 Man 结构体公开

--- database/sqldb/message.go
+++ database/sqldb/message.go
@@ -7,26 +7,26 @@ import (
 	"gorm.io/gorm"
 )
 
-type messageMan struct {
-	db *gorm.DB
+type MessageMan struct {
+	DB *gorm.DB
 }
 
-func (m *messageMan) AddMessage(message *model.Message) error {
+func (m *MessageMan) AddMessage(message *model.Message) error {
 	if message == nil {
 		return errors.New("AddMessage message空指针")
 	}
 	//添加消息
-	if err := m.db.Create(message).Error; err != nil {
+	if err := m.DB.Create(message).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (m *messageMan) QueryMessageRecord(fromUserId int64, toUserId int64, messages *[]model.Message) error {
+func (m *MessageMan) QueryMessageRecord(fromUserId int64, toUserId int64, messages *[]model.Message) error {
 	if messages == nil {
 		return errors.New("QueryMessageRecord messages空指针")
 	}
-	if err := m.db.Model(&model.Message{}).Where("from_user_id = ? and to_user_id = ?", fromUserId, toUserId).Find(messages).Error; err != nil {
+	if err := m.DB.Model(&model.Message{}).Where("from_user_id = ? and to_user_id = ?", fromUserId, toUserId).Find(messages).Error; err != nil {
 		return err
 	}
 	return nil
