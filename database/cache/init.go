5b59175bf74b5728f29581b278eb0304cbd59e07
liulongxin <77443780+1474545380@users.noreply.github.com>
2023-02-21 21:23:32 +0800

    用redis实现点赞接口

--- database/cache/init.go
+++ database/cache/init.go
@@ -6,40 +6,43 @@ import (
 	"time"
 
 	"github.com/HumXC/simple-douyin/handler/douyin"
+	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
 )
 
-type Favorite struct {
+type FavoriteMan struct {
 	thumbsUpActions map[string]int32
 	thumbsUpNum     map[int64]int64
 }
 
-func (c *Favorite) Action(videoID, userID int64, actionType int32) error {
+func (r *RCache) Action(c *gin.Context, videoID, userID int64, actionType int32) error {
 	key := strconv.FormatInt(videoID, 10) + "." + strconv.FormatInt(userID, 10)
-	t, ok := c.thumbsUpActions[key]
-	if !ok {
-		c.thumbsUpActions[key] = actionType
-		c.thumbsUpNum[videoID] = c.thumbsUpNum[videoID] + 1
+	getAction, err := r.rdb.Get(c, key).Result()
+	if err == redis.Nil {
+		r.rdb.Set(c, key, 1, time.Hour*24).Err()
 		return nil
 	}
 	// 点赞情况没有变化，什么时候会出现这种情况呢
-	if t == actionType {
+	intAction, err := strconv.ParseInt(getAction, 10, 32)
+	if int32(intAction) == actionType {
 		return fmt.Errorf("点赞数量没有变化: %d", actionType)
 	}
-	c.thumbsUpActions[key] = actionType
+	if err != redis.Nil && err != nil {
+		panic(fmt.Errorf("点赞错误"))
+	}
 	// 点赞还是取消点赞
 	switch actionType {
 	case 1:
-		c.thumbsUpNum[videoID] = c.thumbsUpNum[videoID] + 1
+		r.rdb.Incr(c, key)
 	case 2:
-		c.thumbsUpNum[videoID] = c.thumbsUpNum[videoID] - 1
+		r.rdb.Decr(c, key)
 	}
 	return nil
 }
-func (c *Favorite) Count(vdeoID int64) int64 {
+func (c *FavoriteMan) Count(vdeoID int64) int64 {
 	return c.thumbsUpNum[vdeoID]
 }
-func (c *Favorite) Sync(duration time.Duration, syncFunc func() error) {
+func (c *FavoriteMan) Sync(duration time.Duration, syncFunc func() error) {
 	t := time.NewTimer(duration)
 	go func(t *time.Timer) {
 		for {
@@ -57,10 +60,10 @@ type RCache struct {
 func NewDouyinRDB(rdb *redis.Client) (*douyin.RDBMan, error) {
 	if rdb == nil {
 		return &douyin.RDBMan{
-			Favorite: &Favorite{
-				thumbsUpActions: make(map[string]int32),
-				thumbsUpNum:     make(map[int64]int64),
-			},
+			// Favorite: &FavoriteMan{
+			// 	thumbsUpActions: make(map[string]int32),
+			// 	thumbsUpNum:     make(map[int64]int64),
+			// },
 		}, nil
 	}
 	return nil, nil
