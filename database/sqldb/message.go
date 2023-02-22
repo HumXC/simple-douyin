35b01dcd4491565375a2e8f6a1608f54aaa0a781
HumXC <Hum-XC@outlook.com>
2023-02-22 12:05:09 +0800

    公开 MessageMan database/sqldb/message.go

--- database/sqldb/message.go
+++ database/sqldb/message.go
@@ -2,30 +2,31 @@ package sqldb
 
 import (
 	"errors"
+
 	"github.com/HumXC/simple-douyin/model"
 	"gorm.io/gorm"
 )
 
-type messageMan struct {
-	db *gorm.DB
+type MessageMan struct {
+	DB *gorm.DB
 }
 
-func (m *messageMan) AddMessage(message *model.Message) error {
+func (m *MessageMan) AddMessage(message *model.Message) error {
 	if message == nil {
 		return errors.New("AddMessage message空指针")
 	}
 	//添加消息
-	if err := m.db.Create(message).Error; err != nil {
+	if err := m.DB.Create(message).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (m *messageMan) QueryChat(fromUserId int64, toUserId int64, time string, messages *[]model.Message) error {
+func (m *MessageMan) QueryChat(fromUserId int64, toUserId int64, time string, messages *[]model.Message) error {
 	if messages == nil {
 		return errors.New("QueryMessageRecord messages空指针")
 	}
-	if err := m.db.Model(&model.Message{}).Where("created_at > ? and ((from_user_id = ? and to_user_id = ?) or (from_user_id = ? and to_user_id = ?))", time, fromUserId, toUserId, toUserId, fromUserId).Find(messages).Error; err != nil {
+	if err := m.DB.Model(&model.Message{}).Where("created_at > ? and ((from_user_id = ? and to_user_id = ?) or (from_user_id = ? and to_user_id = ?))", time, fromUserId, toUserId, toUserId, fromUserId).Find(messages).Error; err != nil {
 		return err
 	}
 	return nil
